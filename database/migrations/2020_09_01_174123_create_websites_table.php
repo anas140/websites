c738620840c5049ed3ccca700c78d1f5306fe500
Muhammed Anas <muhammedanas4140@gmail.com>
2020-09-01 23:17:01 +0530

    websites table structure added

---
+++ database/migrations/2020_09_01_174123_create_websites_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWebsitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('websites', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 32);
+            $table->text('text');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('websites');
+    }
+}
